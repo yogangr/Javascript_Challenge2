af88f3a3f5babef8b902b43561d97ff19787a8be
yogangr <ynugroho487@gmail.com>
2023-03-10 01:12:37 +0700

    mengatur filter cars by availability

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -3,10 +3,12 @@ function filterCarByAvailability(cars) {
   console.log(cars);
 
   // Tempat penampungan hasil
-  const result = [];
+  const result = [...cars];
 
   // Tulis code-mu disini
+  const filter = result.filter((a) => a.available === true);
+  console.log(filter);
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
-  return result;
+  return filter;
 }

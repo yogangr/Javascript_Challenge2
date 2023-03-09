fa4dcc399cda532d9d170c71f749c23a8e41be32
yogangr <ynugroho487@gmail.com>
2023-03-10 01:15:37 +0700

    mengatur sort by year ascendingly

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -7,6 +7,11 @@ function sortCarByYearAscendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
+  result.sort(function (a, b) {
+    return a.year - b.year;
+  });
+
+  console.log(result);
 
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;

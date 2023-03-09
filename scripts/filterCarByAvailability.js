971fd650ffc3676ce6b64e935d59511e282191af
yogangr <ynugroho487@gmail.com>
2023-03-10 01:08:44 +0700

    first commit

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,12 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
